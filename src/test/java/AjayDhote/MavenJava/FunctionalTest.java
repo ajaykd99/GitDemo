dcc1b15605b35b76a5565fabf6d47cdb7d7da7a8
AjayDhote <ajaydhote999@gmail.com>
2020-10-28 18:37:29 +0530

    Commit message from ISt time

--- src/test/java/AjayDhote/MavenJava/FunctionalTest.java
+++ src/test/java/AjayDhote/MavenJava/FunctionalTest.java
@@ -7,6 +7,11 @@ public class FunctionalTest {
 	@Test
 	public void test01() {
 		System.out.println("test 1 pass");
+		System.out.println("test 2 pass");
+		System.out.println("test 3 pass");
+		System.out.println("test 4 pass");
+		System.out.println("test 5 pass");
+		
 	}
 	@Test
 	public void test02() {
