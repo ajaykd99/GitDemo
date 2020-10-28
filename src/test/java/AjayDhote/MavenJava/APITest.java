07bf134cb2e897e62e1af7a4138bf983fcb1f9ce
AjayDhote <ajaydhote999@gmail.com>
2020-10-28 18:57:27 +0530

    Commit message from EST time

--- src/test/java/AjayDhote/MavenJava/APITest.java
+++ src/test/java/AjayDhote/MavenJava/APITest.java
@@ -7,6 +7,8 @@ public class APITest {
 	@Test
 	public void test03() {
 		System.out.println("test 3 pass");
+		System.out.println("test 3 pass");
+		System.out.println("test 3 pass");
 	}
 	@Test
 	public void test04() {
